cd42e974e46117d94a4278a971d758700123ab43
Brian Cherinka <havok2063@hotmail.com>
2017-06-10 15:06:35 -0400

    updating web templates

---
+++ {{cookiecutter.repo_name}}/python/{{cookiecutter.package_name}}/web/controllers/__init__.py
@@ -0,0 +1,43 @@
+# !usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under a 3-clause BSD license.
+#
+
+from __future__ import print_function, division, absolute_import
+from flask_classful import FlaskView
+from flask import request
+
+
+class BaseWebView(FlaskView):
+    ''' This is the Base Web View for all pages '''
+
+    def __init__(self, page):
+        self.base = {}
+        self.base['intro'] = 'Welcome to {{cookiecutter.package_title}}!'
+        self.update_title(page)
+        self._endpoint = self._release = None
+        self._drpver = self._dapver = None
+
+    def before_request(self, *args, **kwargs):
+        ''' this runs before every single request '''
+        self.base['error'] = None
+        self._endpoint = request.endpoint
+
+    def after_request(self, name, response):
+        ''' this runs after every single request '''
+        return response
+
+    def update_title(self, page):
+        ''' Update the title and page '''
+        self.base['title'] = page.title().split('-')[0] if 'main' in page \
+            else page.title().replace('-', ' | ')
+        self.base['page'] = page
+
+    def reset_dict(self, mydict, exclude=None):
+        ''' resets the page dictionary '''
+        exclude = exclude if isinstance(exclude, list) else [exclude]
+        diffkeys = set(mydict) - set(self.base)
+        for key, val in mydict.items():
+            if key in diffkeys and (key not in exclude):
+                mydict[key] = '' if isinstance(val, str) else None
---
+++ {{cookiecutter.repo_name}}/python/{{cookiecutter.package_name}}/web/controllers/index.py
@@ -0,0 +1,36 @@
+# !usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under a 3-clause BSD license.
+#
+
+from __future__ import print_function, division, absolute_import
+from flask import current_app, Blueprint, render_template, jsonify
+from flask import session as current_session, request, redirect, url_for
+from flask_classful import route
+from {{cookiecutter.package_name}}.web.controllers import BaseWebView
+
+
+index = Blueprint("index_page", __name__)
+
+
+class {{cookiecutter.package_title}}(BaseWebView):
+    route_base = '/'
+
+    def __init__(self):
+        super({{cookiecutter.package_title}}, self).__init__('{{cookiecutter.package_name}}-main')
+        self.main = self.base.copy()
+
+    def before_request(self, *args, **kwargs):
+        super(Marvin, self).before_request(*args, **kwargs)
+        self.reset_dict(self.main)
+
+    def index(self):
+        current_app.logger.info('Welcome to {{cookiecutter.package_title}} Web!')
+
+        return render_template("index.html", **self.main)
+
+    def status(self):
+        return 'OK'
+
+{{cookiecutter.package_title}}.register(index)
