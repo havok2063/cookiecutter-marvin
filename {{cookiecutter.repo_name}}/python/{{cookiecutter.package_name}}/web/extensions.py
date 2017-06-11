c977750b2299eb38b153be38a81aa35cf9654670
Brian Cherinka <havok2063@hotmail.com>
2017-06-11 14:14:07 -0400

    adding extensions

---
+++ {{cookiecutter.repo_name}}/python/{{cookiecutter.package_name}}/web/extensions.py
@@ -0,0 +1,13 @@
+# !usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# Licensed under a 3-clause BSD license.
+#
+
+from __future__ import print_function, division, absolute_import
+
+# JS Glue (allows use of Flask.url_for inside javascript)
+import flask_jsglue as jsg
+jsglue = jsg.JSGlue()
+
+
